74199fe939747cde2772d0dc158661fd78bf0f1e
Teki <Teki@DESKTOP-DOJBME5>
2019-05-15 09:58:53 +0700

    agen

--- src/main/java/LinovSupport/Ticketing/controller/AgenController.java
+++ src/main/java/LinovSupport/Ticketing/controller/AgenController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -64,6 +65,11 @@ public class AgenController {
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 		}
+	}
+	
+	@GetMapping("/{email}/{nama}")
+	public ResponseEntity<?> findByFilter(@PathVariable String email,@PathVariable nama){
+		
 	}
 	
 }
