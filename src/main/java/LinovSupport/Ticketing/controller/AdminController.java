03b7165ec57615854d5aedd20edc15fd533ae67b
Raharjo <raharjown@gmail.com>
2019-05-28 11:49:40 +0700

    update admin to user

--- src/main/java/LinovSupport/Ticketing/controller/AdminController.java
+++ src/main/java/LinovSupport/Ticketing/controller/AdminController.java
@@ -1,77 +1,112 @@
-package LinovSupport.Ticketing.controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import LinovSupport.Ticketing.exception.ErrorException;
-import LinovSupport.Ticketing.model.Admin;
-import LinovSupport.Ticketing.service.AdminService;
-
-@CrossOrigin(origins= "*", allowedHeaders= "*")
-@RestController
-@RequestMapping("/admin")
-public class AdminController {
-
-	@Autowired
-	private AdminService adminService;
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<?> findById(@PathVariable String id) {
-		return ResponseEntity.ok(adminService.findById(id));
-	}
-	@GetMapping("/email/{email}")
-	public ResponseEntity<?> findByBk(@PathVariable String email) {
-		return ResponseEntity.ok(adminService.findByBk(email));
-	}
-
-	@GetMapping("")
-	public ResponseEntity<?> findAll() {
-		return ResponseEntity.ok(adminService.findAll());
-	}
-
-	@PostMapping("")
-	public ResponseEntity<?>insertAdmin(@RequestBody Admin admin)
-	 throws ErrorException {
-		String msg;
-		try {
-			adminService.insertAdmin(admin);
-			msg = "success creating admin";
+package LinovSupport.Ticketing.controller;			
+import org.springframework.beans.factory.annotation.Autowired;			
+import org.springframework.http.HttpStatus;			
+import org.springframework.http.ResponseEntity;			
+import org.springframework.web.bind.annotation.CrossOrigin;			
+import org.springframework.web.bind.annotation.DeleteMapping;			
+import org.springframework.web.bind.annotation.GetMapping;			
+import org.springframework.web.bind.annotation.PathVariable;			
+import org.springframework.web.bind.annotation.PostMapping;			
+import org.springframework.web.bind.annotation.PutMapping;			
+import org.springframework.web.bind.annotation.RequestBody;			
+import org.springframework.web.bind.annotation.RequestMapping;			
+import org.springframework.web.bind.annotation.RestController;			
+			
+import LinovSupport.Ticketing.encrypt.BCrypt;			
+import LinovSupport.Ticketing.encrypt.RandomString;			
+import LinovSupport.Ticketing.exception.ErrorException;			
+import LinovSupport.Ticketing.model.Admin;			
+import LinovSupport.Ticketing.model.Agen;			
+import LinovSupport.Ticketing.model.Multi;			
+import LinovSupport.Ticketing.model.User;			
+import LinovSupport.Ticketing.service.AdminService;			
+import LinovSupport.Ticketing.service.UserService;			
+			
+@CrossOrigin(origins= "*", allowedHeaders= "*")			
+@RestController			
+@RequestMapping("/admin")			
+public class AdminController {			
+			
+	@Autowired		
+	private AdminService adminService;		
+	@Autowired		
+	private UserService userService;		
+			
+	private BCrypt bc;		
+			
+			
+	@GetMapping("/{id}")		
+	public ResponseEntity<?> findById(@PathVariable String id) {		
+		return ResponseEntity.ok(adminService.findById(id));	
+	}		
+	@GetMapping("/email/{email}")		
+	public ResponseEntity<?> findByBk(@PathVariable String email) {		
+		return ResponseEntity.ok(adminService.findByBk(email));	
+	}		
+			
+	@GetMapping("")		
+	public ResponseEntity<?> findAll() {		
+		return ResponseEntity.ok(adminService.findAll());	
+	}		
+			
+			
+	@PostMapping("")		
+	public ResponseEntity<?> insertAdmin(@RequestBody Multi admin) throws ErrorException {		
+		String msg;	
+		try {	
+			Admin admint = new Admin();
+			
+			admint.setIdAdmin(admin.getId());
+			admint.setNama(admin.getNama());
+			admint.setEmail(admin.getEmail());
+			admint.setActive(admin.isActive());
+			
+			adminService.insertAdmin(admint);
+			
+			Admin newAdmin = new Admin();
+			newAdmin = adminService.findByBk(admin.getEmail());
+			
+			System.out.println(admin.getRole());
+			RandomString randomString = new RandomString();
+			String pass = admin.getPassword();
+			String encrypt = BCrypt.hashpw(pass,bc.gensalt());
+			User user = new User();
+			user.setUsername(admin.getUsername());
+			user.setPassword(encrypt);
+			user.setIdRole(admin.getRole());
+			user.setDetailRole(newAdmin.getIdAdmin());
+			
+			
+			userService.insertUser(user);
+			
+			msg = "Data berhasil di tambah";
 			return ResponseEntity.ok(msg);
-			
-		} catch(Exception e) {
+		} catch (Exception e) {	
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}	
+	}		
 			
-		}
-	}
-	@PutMapping("")
-	public ResponseEntity<?> updateAdmin(@RequestBody Admin admin) throws ErrorException{
-		String msg;
-		try {
+	@PutMapping("")		
+	public ResponseEntity<?> updateAdmin(@RequestBody Admin admin) throws ErrorException{		
+		String msg;	
+		try {	
 			adminService.updateAdmin(admin);
 			msg="Sucess updating admin";
 			return ResponseEntity.ok(msg);
-		} catch(Exception e) {
+		} catch(Exception e) {	
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 			
-		}
-	}
-	@DeleteMapping("/del/{id}")
-	public ResponseEntity<?>deleteAdmin(@PathVariable String id) {
-		String msg;
-		try {
+		}	
+	}		
+	@DeleteMapping("/del/{id}")		
+	public ResponseEntity<?>deleteAdmin(@PathVariable String id) {		
+		String msg;	
+		try {	
 			adminService.deleteAdmin(id);
 			msg = "success deleting admin";
 			return ResponseEntity.ok(msg);
-		} catch(Exception e) {
+		} catch(Exception e) {	
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-		}
-	}
-}
+		}	
+	}		
+}			
