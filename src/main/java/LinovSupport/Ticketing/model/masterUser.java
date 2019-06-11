82843387351aa9be3350677680b20113228fe2ed
Teki <Teki@DESKTOP-DOJBME5>
2019-06-11 11:42:49 +0700

    update :  login + info

---
+++ src/main/java/LinovSupport/Ticketing/model/masterUser.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package LinovSupport.Ticketing.model;
+
+/**
+ * @author Yosep Teki
+ *
+ */
+public class masterUser {
+	private String idUser;
+	private String username;
+	private String password;
+	private String role;
+	private String nama;
+	private String email;
+	private String usercode;
+	private boolean status;
+	
+	public String getUsercode() {
+		return usercode;
+	}
+	public void setUsercode(String usercode) {
+		this.usercode = usercode;
+	}
+	public String getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(String idUser) {
+		this.idUser = idUser;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	
+	
+
+}
